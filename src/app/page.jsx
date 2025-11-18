723a498c10eab094d45f0a516723186618acaa86
VadymKalashnyk <vadym10107@gmail.com>
2025-11-18 18:19:11 +0200

    add structure for my section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
+import Atmosphere from "@/components/Atmosphere/Atmosphere";
 import Hero from "@/components/Hero/Hero";
 import Image from "next/image";
 
 export default function Home() {
-  return <Hero />;
+  return <Atmosphere />;
 }
---
+++ src/components/Atmosphere/Atmosphere.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Atmosphere = () => {
+  return (
+    <section >
+      <h2 className='text-[20px] font-advent font-bold text-[#E5E5E5] text-center mt-2'>
+        Наша атмосфера
+      </h2>
+
+
+    </section>
+
+  )
+}
+
+export default Atmosphere
+
+
