4a1280df28f038ab7b5f9b24ee7e7ad7b300c7ee
vamykhailo <v.mykhailo.s@gmail.com>
2025-12-11 13:21:37 +0200

    Benefits

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
+import Benefits from "@/components/Benefits/Benefits";
 import Hero from "@/components/Hero/Hero";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
+      <Benefits />
     </>
   );
 };
